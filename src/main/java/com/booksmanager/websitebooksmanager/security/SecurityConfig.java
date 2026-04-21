62d953330cc2c91149f84fe7576fbbba77d3e6cd
mathi <grantzaumathias@gmail.com>
2026-04-21 23:29:36 +0200

    added SecurityConfig, and EmbedPdfViewer

--- src/main/java/com/booksmanager/websitebooksmanager/security/SecurityConfig.java
+++ src/main/java/com/booksmanager/websitebooksmanager/security/SecurityConfig.java
@@ -2,10 +2,8 @@ package com.booksmanager.websitebooksmanager.security;
 
 import com.booksmanager.websitebooksmanager.views.LoginView;
 import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
-import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,24 +22,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.with(VaadinSecurityConfigurer.vaadin(), configurer -> {configurer.loginView(LoginView.class);});
-        http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers(
-                    "/VAADIN/**",
-                    "/frontend/**",
-                    "/icons/**",
-                    "/images/**",
-                    "/*.css",
-                    "/*.js",
-                    "/book.jpg",
-                    "/api/**"
-            ).permitAll();
-
-
-
-
-
-        });
+        http.with(VaadinSecurityConfigurer.vaadin(), configurer -> configurer.loginView(LoginView.class));
+        http.authorizeHttpRequests(auth -> auth.requestMatchers(
+                "/VAADIN/**",
+                "/frontend/**",
+                "/icons/**",
+                "/images/**",
+                "/*.css",
+                "/*.js",
+                "/book.jpg",
+                "/api/**"
+        ).permitAll());
         return http.build();
     }
 
